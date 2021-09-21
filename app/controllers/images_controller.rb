a5806c1d9c6b79b7949073197f13c22e06642e3c
Ascen Salmerón <ascensalmanez@gmail.com>
2021-09-21 19:21:50 +0200

    Add create route

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,4 +5,23 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
   end
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      redirect_to @image
+    else
+      render :new
+    end
+  end
+
+  private
+    def image_params
+      params.require(:image).permit(:name, :path)
+    end
+
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,4 @@
 class Image < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :path, presence: true
 end
